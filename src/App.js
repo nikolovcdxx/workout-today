b5c78eda7332ba524c679e06a4893fd338cc3138
nikolovcdxx <nikolovcdxx@gmail.com>
2023-03-22 22:21:23 +0200

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={ <h2>SNIMKA NA SHTANGI HERE /// if no logged user "nav div.user - display: none;"</h2>}/>
+        <Route path="/catalog" element={ <h2>TRENIROVKATA NA VSEKI CHOBANIN</h2>}/>
+        <Route path="/create" element={ <h2>MAINE TRENIROVKEN CREATE</h2>}/>
+        <Route path="/login" element={ <h2>LOGIN FORMA HERE</h2>}/>
+        <Route path="/register" element={ <h2>REGISTER FORMA HERE</h2>}/>
+        <Route path="/profile" element={ <h2>TRENIROVKATA ZA DNES</h2>}/>
+        <Route path="*" element={ <h2>404 NOT FOUND MADAFAKA</h2>}/>
+        
+      </Routes>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,32 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Navigation.module.css';
 
 
 export default function Navigation() {
     return (
         <nav>
-            <a className={styles.active} href="/">
+            <Link className={styles.active} to="/">
                 Fitness
-            </a>
-            <a href="/">All Workouts Today</a>
+            </Link>
+            <Link to="/catalog">All Workouts Today</Link>
             {/* Logged users */}
             <div className={styles.user}>
-                <a href="/">Create Your Workout</a>
+                <Link to="/create">Create Your Workout</Link>
                 <div className={styles.profile}>
                     <span>
                         Welcome, {'{'}username{'}'}
                     </span>
-                    <a href="/">My Workout</a>
-                    <a href="/">Logout</a>
+                    <Link to="/profile">My Workout</Link>
+                    <Link to="/logout">Logout</Link>
                 </div>
             </div>
             {/* Guest users */}
 
             <div className={styles.guest}>
                 <div className={styles.profile}>
-                    <a href="/">Login</a>
-                    <a href="/">Register</a>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
                 </div>
 
             </div>
